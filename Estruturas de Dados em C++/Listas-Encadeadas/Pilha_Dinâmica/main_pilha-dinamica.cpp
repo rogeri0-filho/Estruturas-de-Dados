fc9be6ad9d868797add0734b34c0961fe9d3e6bf
Rogério <rogeriofilho63@gmail.com>
2023-06-14 10:13:13 -0300

    Programa principal

---
+++ Estruturas de Dados em C++/Listas-Encadeadas/Pilha_Dinâmica/main_pilha-dinamica.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "pilha-dinamica.h"
+
+using namespace std;
+
+int main(){
+    PilhaDinamica pilha_1;
+    TipoItem var_item;
+    int opc;
+    cout << "Programa gerador de pilhas:\n\n";
+
+    do {
+        cout << "[0] - Parar.\n";
+        cout << "[1] - Inserir.\n";
+        cout << "[2] - Remover.\n";
+        cout << "[3] - Imprimir.\n"; 
+        cout << "[4] - Ler se a pilha esta vazia.\n";
+        cout << "[5] - Ler se a pilha esta cheia.\n";   
+
+        cout << "Digite sua opcao: ";
+        cin >> opc;
+        cout << endl;
+
+        if (opc == 1){
+            cout << "Digite o elemento a ser inserido: ";
+            cin >> var_item;
+            pilha_1.insert(var_item);
+            cout << endl; 
+
+        } else if (opc == 2){
+            var_item = pilha_1.remover();
+            cout << "Elemento removido: " << var_item << endl;
+
+        } else if (opc == 3){
+            pilha_1.imprimir();
+
+        } else if (opc == 4){
+            cout << pilha_1.is_empty() << endl;
+
+        } else if (opc ==5){
+            cout << pilha_1.is_full() << endl;
+        }
+
+    } while(opc != 0);
+    return 0;
+}
